ab1e295f9e7c21624b48cc69acd00ee08819104d
Pascal Z <1606056+IcyFeVA@users.noreply.github.com>
2024-05-17 09:17:56 -0700

    Made it work Made my own component woohoo!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from './components/Hello';
 
 function App() {
+
+  const user = {
+    yourName: "Jonathan",
+    age: 25,
+    loggedIn: true,
+    email: "LZ4wH@example.com",
+    hasPaid: false
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Hello userObj={user} />
+    </div >
   );
 }
 
 export default App;
+
+
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+
+function Hello({ userObj }) {
+    return (
+        <div>
+            <h1>Hello {userObj.yourName} Age: {userObj.age}</h1>
+        </div>
+    );
+}
+
+export default Hello;
+
